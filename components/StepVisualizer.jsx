e2c36bc19b790e590f5b0a06e2284767a451fc53
jon-jh <hiebert456@gmail.com>
2024-12-18 14:01:49 -0600

    Complete gold calculator and display for single current strava user.

--- components/StepVisualizer.jsx
+++ components/StepVisualizer.jsx
@@ -1,14 +1,17 @@
+"use client"; // Mark this component as a Client Component
+
 import "./StepVisualizer.css";
 import Avatar from "./Avatar";
+import { useEffect, useState } from "react";
 
-const StepVisualizer = ({
+export default function StepVisualizer({
   userCharacter,
   hat,
   shirt,
   pants,
   boots,
   weapon,
-}) => {
+}) {
   // Deconstruct stats from userCharacter...
   let { name, distance_travelled_today } = userCharacter;
 
@@ -16,6 +19,30 @@ const StepVisualizer = ({
   if (distance_travelled_today > 1000) {
     distance_travelled_today = 1000;
   }
+
+  // fetchGold to fetch gold amount from the database
+  const [gold, setGold] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/fetchGold", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userName: name }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.gold !== undefined) {
+          setGold(data.gold);
+        } else {
+          console.error("Failed to fetch gold value");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching gold value:", error);
+      });
+  }, [name]);
 
   // Calculate the position of the character based on distance travelled...
   const position = 10 + (distance_travelled_today / 1000) * 80; // 0/1000 = left:10% and 1000/1000 = left:90%
@@ -25,6 +52,7 @@ const StepVisualizer = ({
     <div className="step-visualizer">
       <h1>{name}</h1>
       <h2>{distance_travelled_today}/1000</h2>
+      <h2>{gold}🪙</h2>
       <div
         className="step-visualizer-character"
         style={{ left: `${position}%` }}
@@ -36,10 +64,7 @@ const StepVisualizer = ({
           boots={boots}
           weapon={weapon}
         />
-        {/* Replace above emoji with avatar */}
       </div>
     </div>
   );
-};
-
-export default StepVisualizer;
+}
