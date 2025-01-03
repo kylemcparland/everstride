6003be8c7db3afa7c0f3429beab4577a2222fa0c
Kyle McParland <kylemcparland@gmail.com>
2025-01-03 17:21:50 -0500

    Add font-awesome icon as dependency. Update Store CSS to accommodate Stripe button/banner.

--- components/PurchaseGoldStripe.jsx
+++ components/PurchaseGoldStripe.jsx
@@ -1,11 +1,16 @@
 import "./PurchaseGoldStripe.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const PurchaseGoldStripe = () => {
   return (
     <div className="PurchaseGoldStripe">
-      <span>Purchase 20 Gold for only $5!</span>
+      <div className="PurchaseGoldStripe-ad">
+        <h3>Purchase 20 Gold for only $5!</h3>
+      </div>
       <a href="/purchase-gold" className="PurchaseGoldStripe-link">
         <button className="PurchaseGoldStripe-button">
+          <FontAwesomeIcon icon={faCartShopping} size="2x" />
           <h2>Secure payment with Stripe</h2>
         </button>
       </a>
