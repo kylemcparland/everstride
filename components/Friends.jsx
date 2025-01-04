db8139f1df221ff3e8a7be62c40e49880008443d
jon-jh <hiebert456@gmail.com>
2025-01-04 15:20:01 -0600

    Change handleSubmit to accept the ID of the adding friend, instead of waiting for the event. Remove unused code.

--- components/Friends.jsx
+++ components/Friends.jsx
@@ -6,7 +6,7 @@ const Friends = ({ currentUserId }) => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState("");
 
-// Load all users from database
+  // Load all users from database
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -30,15 +30,8 @@ const Friends = ({ currentUserId }) => {
     }
   }, [currentUserId]);
 
-// Add friend button
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (selectedUser === "") {
-      alert("Choose someone to add as a friend");
-      return;
-    }
-
+  // Add friend button
+  const handleSubmit = async (userId) => {
     try {
       const response = await fetch("/api/addFriend", {
         method: "POST",
@@ -47,7 +40,7 @@ const Friends = ({ currentUserId }) => {
         },
         body: JSON.stringify({
           userId1: currentUserId,
-          userId2: selectedUser,
+          userId2: userId,
         }),
       });
 
@@ -60,57 +53,34 @@ const Friends = ({ currentUserId }) => {
       alert("Failed");
     }
   };
+  return (
+    <UserCards
+      users={users}
+      currentUserId={currentUserId}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
 
-// Actual list of available users
-//   return (
-//     <div className="container">
-//       <p>Users</p>
-//       <form onSubmit={handleSubmit}>
-//         {/* <select> forms a drop down list */}
-//         <label>
-//           Select User:
-//           <select
-//             value={selectedUser}
-//             onChange={(e) => setSelectedUser(e.target.value)}
-//           >
-//             <option value="">Select a user</option>
-//             {users
-//             // Simple filter to not show yourself in the list
-//               .filter((user) => user.id !== currentUserId) 
-//               .map((user) => (
-//                 <option key={user.id} value={user.id}>
-//                   {`${user.name} (${user.id})`}
-//                 </option>
-//               ))}
-//           </select>
-//         </label>
-//         <button type="submit">Add Friend</button>
-//       </form>
-//     </div>
-//   );
-// };
-// Replacing the drop down list with static user cards
-return (
-  <UserCards />
-)
-}
-
-const UserCards = () => {
-return (
-  <div className = 'container'>
-    <p>Users</p>
-    <div className = 'users'>
-      <div className = 'card'>
-        <h3>Name: </h3>
-        <p>ID: </p>
-        <p>Distance: </p>
-        <p>Location: </p>
-        <button></button>
+const UserCards = ({ users, currentUserId, handleSubmit }) => {
+  return (
+    <div className="container">
+      <p>Users</p>
+      <div className="users">
+        {users
+          .filter((user) => user.id !== currentUserId)
+          .map((user) => (
+            <div className="card" key={user.id}>
+              <h3>Name: {user.name}</h3>
+              <p>ID: (Don't really need)</p>
+              <p>Distance: (Distance run total?)</p>
+              <p>Location: (Location in game)</p>
+              <button onClick={() => handleSubmit(user.id)}>Add Friend</button>
+            </div>
+          ))}
       </div>
     </div>
-  </div>
-);
-}
-
+  );
+};
 
 export default Friends;
