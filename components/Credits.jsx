6a690dfd03be3bdf32ef579d14ae881ac98d1fe1
jon-jh <hiebert456@gmail.com>
2025-01-14 16:10:17 -0600

    Adjusted credits section a little

--- components/Credits.jsx
+++ components/Credits.jsx
@@ -3,28 +3,32 @@ import "./Credits.css";
 const Credits = () => {
   return (
     <div className="Credits">
-      <h3>Contributors:</h3>
+      <h3>Contributors</h3>
       {/* KYLE */}
       <b>Kyle McParland</b>
       <div className="links">
-        <a href="https://www.linkedin.com/in/kyle-mcparland/">LinkedIn</a>
-        <a href="https://github.com/kylemcparland">GitHub</a>
+        <a href="https://www.linkedin.com/in/kyle-mcparland/">
+          LinkedIn kyle-mcparland
+        </a>
+        <a href="https://github.com/kylemcparland">GitHub /kylemcparland</a>
       </div>
 
       {/* JON */}
       <b>Jon Hiebert</b>
       <div className="links">
-        <a href="https://www.linkedin.com/in/jonathan-h-8103b2289/">LinkedIn</a>
-        <a href="https://github.com/jon-jh">GitHub</a>
+        <a href="https://www.linkedin.com/in/jonathan-h-8103b2289/">
+          LinkedIn jonathan-h-8103b2289
+        </a>
+        <a href="https://github.com/jon-jh">GitHub /jon-jh</a>
       </div>
 
       {/* BEN */}
       <b>Ben Hallam</b>
       <div className="links">
         <a href="https://www.linkedin.com/in/benjamin-hallam-195a10309/">
-          LinkedIn
+          LinkedIn benjamin-hallam-195a10309
         </a>
-        <a href="https://github.com/bendhallam">GitHub</a>
+        <a href="https://github.com/bendhallam">GitHub /bendhallam</a>
       </div>
     </div>
   );
