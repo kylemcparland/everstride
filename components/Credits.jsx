1fcd9c68d896b27e3b3bce97c3ffb20edf05e195
jon-jh <hiebert456@gmail.com>
2025-01-16 14:36:01 -0600

    Update my linkedIn url name, text shadow

--- components/Credits.jsx
+++ components/Credits.jsx
@@ -23,7 +23,7 @@ const Credits = () => {
           className="link"
           href="https://www.linkedin.com/in/jonathan-h-8103b2289/"
         >
-          linkedin/jonathan-h
+          linkedin/jonathan-dh
         </a>
         |
         <a className="link" href="https://github.com/jon-jh">
