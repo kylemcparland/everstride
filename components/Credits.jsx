4e2dedde13ba6e5b5052d4118a13dddde36e4f9b
Kyle McParland <kylemcparland@gmail.com>
2025-01-16 14:15:49 -0500

    Adjust Credits CSS.

--- components/Credits.jsx
+++ components/Credits.jsx
@@ -7,24 +7,43 @@ const Credits = () => {
       {/* KYLE */}
       <b>Kyle McParland</b>
       <div className="links">
-        <a className="link" href="https://www.linkedin.com/in/kyle-mcparland/">linkedin/kyle-mcparland</a>
-        <a className="link" href="https://github.com/kylemcparland">github/kylemcparland</a>
+        <a className="link" href="https://www.linkedin.com/in/kyle-mcparland/">
+          linkedin/kyle-mcparland
+        </a>
+        |
+        <a className="link" href="https://github.com/kylemcparland">
+          github/kylemcparland
+        </a>
       </div>
 
       {/* JON */}
       <b>Jon Hiebert</b>
       <div className="links">
-        <a className="link" href="https://www.linkedin.com/in/jonathan-h-8103b2289/">linkedin/jonathan-h</a>
-        <a className="link" href="https://github.com/jon-jh">gitHub/jon-jh</a>
+        <a
+          className="link"
+          href="https://www.linkedin.com/in/jonathan-h-8103b2289/"
+        >
+          linkedin/jonathan-h
+        </a>
+        |
+        <a className="link" href="https://github.com/jon-jh">
+          github/jon-jh
+        </a>
       </div>
 
       {/* BEN */}
       <b>Ben Hallam</b>
       <div className="links">
-        <a className="link" href="https://www.linkedin.com/in/benjamin-hallam-195a10309/">
+        <a
+          className="link"
+          href="https://www.linkedin.com/in/benjamin-hallam-195a10309/"
+        >
           linkedin/benjamin-hallam
         </a>
-        <a className="link" href="https://github.com/bendhallam">gitHub/bendhallam</a>
+        |
+        <a className="link" href="https://github.com/bendhallam">
+          github/bendhallam
+        </a>
       </div>
     </div>
   );
